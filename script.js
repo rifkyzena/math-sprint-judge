e6890ef934208a28b0089c5cd53f315ca0250182
frazeinky <rifkyzena26@gmail.com>
2022-11-17 20:55:14 +0700

    question amount

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const penaltyTimeEl = document.querySelector('.penalty-time');
 const playAgainBtn = document.querySelector('.play-again');
 
 // Equations
-
+let questionAmount = 0;
 let equationsArray = [];
 
 // Game Page
@@ -82,3 +82,37 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+
+// Get the value from selected radio button
+// so that only one is selected as always
+function getRadioValue(){
+  let radioValue;
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked){
+      radioValue = radioInput.value;
+    }
+  });
+  // outside the if and below foreach
+  return radioValue;
+}
+
+// Form that decides amount of questions
+function selectQuestionAmount(e){
+  e.preventDefault();
+  questionAmount = getRadioValue();
+  console.log('question amount:', questionAmount);
+}
+
+startForm.addEventListener('click', () => {
+  radioContainers.forEach((radioEl) => {
+    // Remove Selected Label Styling
+    radioEl.classList.remove('selected-label');
+    // Add it back if radio input is checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add('selected-label');
+    }
+  });
+});
+
+// Event Listeners
+startForm.addEventListener('submit', selectQuestionAmount);
